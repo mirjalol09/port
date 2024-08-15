ee699a108f637bcc802c91e5a9714fa57e615225
mirjalol09 <akbarovmirjalol998@gmail.com>
2024-08-15 21:35:07 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// const ElBtn = document.querySelector('.header-btn');
+// const nest = document.querySelector('.text-box');
+
+// ElBtn.addEventListener('click', function(){
+//     document.body.classList.toggle('dark_mode');
+// })
+// ElBtn.addEventListener('click', function(){
+//     document.body.classList.toggle('dark_mode');
+// })
+// // const ElBtn2 = document.querySelector('.ami-btn')
+// // const host = document.querySelector('.ami-h1')
+// // ElBtn2.addEventListener('click', function(){
+// //     host.textContent = 'I am the Web  Developer'
+// // })
+const ElBtn = document.querySelector('.header-btn');
+
+ElBtn.addEventListener('click', function(){
+ document.body.classList.toggle('dark_mode');
+})
